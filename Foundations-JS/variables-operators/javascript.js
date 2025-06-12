13b702b81a3b472878aa4879ac7dfa73e9bc24e9
Maaz Khan <63423339+Maaz-R-Khan@users.noreply.github.com>
2025-06-12 12:15:37 -0400

    completed assignment for variables and operators lesson

---
+++ Foundations-JS/variables-operators/javascript.js
@@ -0,0 +1,37 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let x = 10; 
+
+x = 11; 
+console.log(x); //variables can be reassigned
+
+const y = 12; 
+// y = 13; 
+// console.log(y); //this will give an error since const values cannot be reassigned. 
+
+
+// Assignment: 
+
+
+// Add 2 Numbers: 
+console.log(23 + 97); 
+
+//Add 6 different numbers: 
+console.log(1 + 2 + 3 + 4 + 5 + 6); 
+
+let a = 10; 
+console.log(a); //prints 10, old value
+a = 13; 
+console.log(a); //prints 13, new value since its reassigned. 
+
+let b = 7 * a; 
+console.log(b); //prints 91
+
+const max = 57; 
+const actual = max - 13; 
+const percentage = actual / max; 
+console.log(percentage); 
